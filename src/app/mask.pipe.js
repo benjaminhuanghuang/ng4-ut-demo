e6a87b194fcb86eb20c373d6a26275fe3f8b23b2
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-11 08:19:04 -0700

    jasmine html page

---
+++ src/app/mask.pipe.js
@@ -0,0 +1,16 @@
+class MaskPipe {
+
+  transform(value, selection) {
+    let masked = '';
+    for (let i = 0; i < value.length; i++) {
+      const c = value.charAt(i);
+      if (selection.includes(c)) {
+        masked += c;
+      } else {
+        masked += '*';
+      }
+    }
+    return masked;
+  }
+
+}
