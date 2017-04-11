7dbb265d06653ae6831b2359952b97f8534b9dc5
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-11 00:42:15 -0700

    add karma

--- src/specs.ts
+++ src/specs.ts
@@ -2,4 +2,10 @@ import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone'; 
 
-import './app/mask.pipe.spec';
+//import './app/mask.pipe.spec.ts';
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+// need "@types/webpack":"^2.2.14",
+requireAll(require.context('./app', true, /\.spec\.ts$/));
