b657514ea153ec59496dc2e0444eeeea8b148b40
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-11 17:29:02 -0700

    test component template

--- src/app/letter-buttons.component.spec.ts
+++ src/app/letter-buttons.component.spec.ts
@@ -4,7 +4,7 @@ import { LetterButtonsComponent } from './letter-buttons.component';
 
 describe('LetterButtonsComponent', () => {
   beforeEach(() => {
-     TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       declarations: [
         LetterButtonsComponent
       ],
@@ -16,6 +16,7 @@ describe('LetterButtonsComponent', () => {
   });
 
   const component = new LetterButtonsComponent();
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
   it('test alphabet', () => {
     // check the content using toEqual
@@ -26,11 +27,37 @@ describe('LetterButtonsComponent', () => {
     expect(component.alphabet[0]).toBe('A');
   });
 
-  fit('should display a button using TestBed', () => {
+  it('should display a button using TestBed', () => {
     const fixture = TestBed.createComponent(LetterButtonsComponent);
+    fixture.autoDetectChanges();
     const component = fixture.componentInstance;
-    const element = fixture.nativeElement;
-    console.log('element', element);
+    const element = <HTMLElement>fixture.nativeElement;
+    console.log(element);
+    const buttons = element.querySelectorAll("button")
+    expect(buttons.length).toBe(26);
+    for (let i = 0; i < alphabet.length; i++) {
+      const button = buttons.item(i);
+      expect(button.textContent).toBe(alphabet.charAt(i));
+      expect(button.disabled).toBe(false);
+    }
   });
 
+  it('should add letter to selection when a button is clicked', () => {
+    const fixture = TestBed.createComponent(LetterButtonsComponent);
+    fixture.autoDetectChanges();
+    const component = fixture.componentInstance;
+    const element = <HTMLElement>fixture.nativeElement;
+    console.log(element);
+    const buttons = element.querySelectorAll("button")
+    const buttonO = buttons.item(alphabet.indexOf('O'))
+    const buttonK = buttons.item(alphabet.indexOf('K'))
+
+    expect(component.selection).toBe('');
+    buttonO.click();
+    expect(component.selection).toBe('O');
+    expect(buttonO.disabled).toBe(true);
+    buttonK.click();
+    expect(component.selection).toBe('OK');
+    expect(buttonK.disabled).toBe(true);
+  });
 });
